9b24003397c71d79191ee84b6bfa18f0ed3fa9cf
Wicho22luis <luis.f.jasso22@gmail.com>
2023-11-17 20:20:48 -0600

    First Test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\Http\Controllers\Operations_controller;
 
 class ExampleTest extends TestCase
 {
@@ -12,5 +13,10 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+    public function test_suma(): void{
+        $instance = new Operations_controller();
+        $result=$instance->suma(4,6);
+        $this->assertEquals(10,$result);
     }
 }
