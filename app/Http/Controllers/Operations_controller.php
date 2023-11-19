e52b90217adad76ad4a937de45d53e6d6816334e
Wicho22luis <luis.f.jasso22@gmail.com>
2023-11-19 02:29:16 -0600

    Nuevos Tests

--- app/Http/Controllers/Operations_controller.php
+++ app/Http/Controllers/Operations_controller.php
@@ -10,7 +10,51 @@ class Operations_controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    public function suma($a,$b){
+    public function suma($a, $b){
         return $a + $b;
+    }
+
+    function esBisiesto($year){
+        if ($year % 4 != 0) {
+            return false;
+        } else if ($year % 4 == 0 && $year % 100 != 0) {
+            return "esBiciesto";
+        } else if ($year % 4 == 0 && $year % 100 == 0 && $year % 400 != 0) {
+            return false;
+        } else if ($year % 4 == 0 && $year % 100 == 0 && $year % 400 == 0) {
+            return "esBiciesto";
+        }
+    }
+
+    public function filtrarNumerosPares($lista){
+        $listaFiltrada = [];
+        for ($i = 0; $i < count($lista); $i++) {
+            if ($lista[$i]%2 == 0) {
+                array_push($listaFiltrada, $lista[$i]);
+            }
+        }
+        return $listaFiltrada;
+    }
+
+    public function CalcularPrecioPublico($costoProducto, $tipoProducto){
+        if ($tipoProducto == "Alimentos") {
+            $precioPublico = $costoProducto * 1.40;
+        } else if ($tipoProducto == "Tecnologia") {
+            $precioPublico = $costoProducto * 1.35;
+        } else {
+            $precioPublico = $costoProducto * 1.20;
+        }
+        return $precioPublico;
+    }
+
+    public function concatenarArreglos($array1, $array2){
+        $arrayFinal = array_merge($array1, $array2);
+        return $arrayFinal;
+    }
+
+
+    public function ordenarArregloNumerico($arregloNumerico){
+        sort($arregloNumerico);
+        return $arregloNumerico;
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,8 +15,64 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
     public function test_suma(): void{
+        
         $instance = new Operations_controller();
         $result=$instance->suma(4,6);
-        $this->assertEquals(11,$result);
+        $this->assertEquals(10,$result);
+    }
+
+    public function test_esBiciesto(): void{
+        $instance = new Operations_controller();
+        $result = $instance->esBisiesto(2020);
+
+        $this->assertEquals($result,"esBiciesto");
+        $this->assertIsString($result);
+        $this->assertIsNotBool($result);
+        $this->assertIsNotNumeric($result);
+        $this->assertStringContainsString("Biciesto",$result);
+             
+    }
+    public function test_filtrarNumerosPares(): void{
+        $instance = new Operations_controller();
+        $arregloNumerico = array(1,2,3,4,5,6,7,8,9,10);
+        $result = $instance->filtrarNumerosPares($arregloNumerico);
+        $this->assertEquals($result, array(2,4,6,8,10));
+        $this->assertContains(4, $result);
+        $this->assertEquals(count($result), 5);
+        $this->assertIsArray($result);
+        $this->assertIsIterable($result);
+    }
+    public function test_CalcularPrecioPublico(): void{
+        $instance = new Operations_controller();
+        $result = $instance->CalcularPrecioPublico(100, "Alimentos");
+        $this->assertEquals($result, 140);
+        $this->assertIsNumeric($result);
+        $this->assertGreaterThan(100,$result);
+        $this->assertIsNotString($result);
+        $this->assertLessThan(200,$result);
+       
+             
+    }
+    public function test_concatenarArreglos(): void{
+        $instance = new Operations_controller();
+        $arreglo1 = array(1,2,3,4);
+        $arreglo2 = array(5,6,7,8);
+        $result = $instance->concatenarArreglos($arreglo1,$arreglo2);
+        $this->assertEquals($result, array(1,2,3,4,5,6,7,8));
+        $this->assertIsArray($result);
+        $this->assertGreaterThan(4,count($result));
+        $this->assertContains(8,$result);
+        $this->assertIsNotObject($result);
+             
+    }
+    public function test_ordenarArregloNumerico(): void{
+        $instance = new Operations_controller();
+        $arregloNumerico = array(3,500,2,12,186,421,999,89);
+        $result = $instance->ordenarArregloNumerico($arregloNumerico);
+        $this->assertEquals($result[0],2);
+        $this->assertContains(500, $arregloNumerico);
+        $this->assertIsArray($result);
+        $this->assertIsNotBool($result);
+        $this->assertIsIterable($result);
     }
 }
